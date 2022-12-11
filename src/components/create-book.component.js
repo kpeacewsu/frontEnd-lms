68dc65909f42dbdd4baed757472ea811d5031a01
Kriti <kpeacewsu@gmail.com>
2022-12-11 01:40:33 -0500

    Update create-book.component.js

--- src/components/create-book.component.js
+++ src/components/create-book.component.js
@@ -12,9 +12,7 @@ export default class CreateBook extends Component {
       author: '',
       price: 0,
       publisher: '',
-      date: '',
-      books: [],
-      authors: []
+      date: ''
     }
   }
 
@@ -22,17 +20,15 @@ export default class CreateBook extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-   const exercise = {
-      username: this.state.username,
-      description: this.state.description,
-      duration: this.state.duration,
-      reps: this.state.reps,
+   const booksInfo = {
+      title: this.state.title,
+      author: this.state.author,
+      price: this.state.price,
+      publisher: this.state.publisher,
       date: this.state.date
     }
 
-    console.log(exercise);
-
-    axios.post('http://localhost:8000/exercises/add', exercise)
+    axios.post('http://localhost:5000/bookRouter/add', booksInfo)
       .then(res => console.log(res.data));
 
     window.location = '/';
@@ -42,73 +38,7 @@ export default class CreateBook extends Component {
     return (
     <div>
       <h3>Log Exercise</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          <label>Member: </label>
-          <select ref="userInput"
-              required
-              className="form-control"
-              value={this.state.username}
-              onChange={this.onChangeUsername}>
-              {
-                this.state.users.map(function(user) {
-                  return <option 
-                    key={user}
-                    value={user}>{user}
-                    </option>;
-                })
-              }
-          </select>
-        </div>
-        <div className="form-group"> 
-          <label>Exercise: </label>
-          <select ref="userInput"
-              required
-              className="form-control"
-              value={this.state.description}
-              onChange={this.onChangeDescription}>
-              {
-                this.state.exercisetypes.map(function(etype) {
-                  return <option 
-                    key={etype}
-                    value={etype}>{etype}
-                    </option>;
-                })
-              }
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Duration (in minutes): </label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.duration}
-              onChange={this.onChangeDuration}
-              />
-        </div>
-        <div className="form-group">
-          <label>Repetations: </label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.reps}
-              onChange={this.onChangeReps}
-              />
-        </div>
-        <div className="form-group">
-          <label>Date: </label>
-          <div>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.onChangeDate}
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <input type="submit" value="Log" className="btn btn-primary" />
-        </div>
-      </form>
+     
     </div>
     )
   }
